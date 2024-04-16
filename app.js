0620dda6885c7f31129a3e2ce9e176b748900ee0
L2216 <L2216@lisboa.epbjc.local>
2024-04-16 14:33:45 +0100

    16-04-2024 desmerdificação

--- app.js
+++ app.js
@@ -1,25 +1,131 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors'
- 
-import { bookingsRouter } from "./routes/bookings.js"
-import { roomsRouter } from "./routes/rooms.js"
-import { authRouter } from "./routes/auth.js"
-
+const express = require('express');
 const app = express();
 const port = 4000;
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const mssql = require('mssql/msnodesqlv8');
+const bcrypt = require('bcrypt');
+
+
+const connection = new mssql.ConnectionPool({
+  database: "Hotel Eden",
+  server: "(localdb)\\MSSQLlocaldb",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true
+  }
+}); "server=(localdb)\\MSSQLlocaldb"
 
 app.use(cors({
   origin: "*"
 }));
-app.use(bodyParser.urlencoded({
+
+app.use(bodyparser.urlencoded({
   extended: false
 }));
-app.use(bodyParser.json());
+app.use(bodyparser.json());
 
-app.use("/reservas", bookingsRouter)
-app.use("/quartos", roomsRouter)
-app.use("/auth", authRouter)
+app.post('/login', async (req, res) => { //pegar data do front para verificar com a data da base de dados
+  console.log("login called")
+  const { identifier, password } = req.body;
+
+  console.log(req);
+  console.log("pass is " + password)
+  console.log("identifier is " + identifier)
+
+  try {
+    await connection.connect(); //conectar com a database
+
+    const result = await connection.query`select * from Clientes where Email=${identifier} and PalavraPasse=${password}`; //checar credenciais
+    console.log(identifier);
+    console.log(password);
+    console.log(result);
+
+    if (result.recordset.length > 0) { //verificar se tem um resultado
+      res.send(result.recordset[0]) //return value
+
+    } else {
+      res.status(401).send('credenciais inválidas')
+    }
+  }
+
+  catch (err) {
+    console.error(err)
+    res.status(500).send('internal server error')
+  }
+
+  finally {
+    await connection.close();
+  }
+})
+
+app.get("/quartos", async (req, res) => {
+  try {
+    await connection.connect();
+    const result = await connection.query`select NumQuarto, Quartos.TipoQuarto, Descricao, Preco, Imagem
+from Quartos 
+inner join TipoQuarto on TipoQuarto.TipoQuarto = Quartos.TipoQuarto`
+    res.json(result.recordset)
+  }
+
+  catch (err) {
+    console.log(res)
+    console.error(err)
+    res.status(500).send('internal server error')
+  }
+
+  finally {
+    await connection.close();
+  }
+}
+)
+
+app.post("/reservar", async (req, res) => {
+  const { numQuarto, dataEntrada, dataSaida, nPessoas, CodCli } = req.body
+  console.log(req)
+
+  try {
+    await connection.connect();
+    console.log(numQuarto)
+
+    if (dataEntrada < dataSaida) {
+      console.log("data aceitável por enquanto")
+      const result1 = await connection.query`select * from QuartosReservas where Numquarto = ${numQuarto} ` //adicionar as datas depois de testar
+      console.log("resultados = " + result1.recordset.length)
+
+      if (result1.recordset.length != 0) { //é ==, testing
+        console.log("if successful")
+
+        //verificar se as datas são aceitaveis
+        const result2 = await connection.query`insert into Reservas (CodCli, NumPessoas, DataEntrada, DataSaida) 
+        values (${CodCli},${nPessoas}, ${dataEntrada}, ${dataSaida})`
+
+        const result3 = await connection.query`insert into QuartosReservas(NumQuarto, DataEntrada, DataSaida) 
+        values (${numQuarto},${dataEntrada},${dataSaida}) `
+
+        const precoQuarto = await connection.query(`select TipoQuarto.Preco from Quartos 
+        inner join TipoQuarto on TipoQuarto.TipoQuarto = Quartos.TipoQuarto
+        where Quartos.NumQuarto = ${numQuarto}`)
+        console.log(precoQuarto.recordsets + numQuarto + " preco do quarto")
+        
+        const result4 = await connection.query`insert into Billing(Bill, CodCli) 
+        values (${precoQuarto}, ${CodCli}) `
+      }
+    }
+  }
+
+  catch {
+    res.status(401).send('credenciais inválidas')
+    console.log(res)
+    //console.err(err)
+    res.status(500).send('internal server error')
+  }
+
+  finally {
+    await connection.close();
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- services/bookings.service.js
+++
@@ -1,9 +0,0 @@
-import query from "./db.service.js";
-
-export async function getAllBookings() {
-    console.log("querying")
-  const bookings = await query(
-    `SELECT * FROM Reservas`,
-  );
-  return bookings;
-}
