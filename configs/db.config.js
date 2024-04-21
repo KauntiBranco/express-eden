a8ed8465079477f7e30c33ce4fa14f21f6274e0f
KauntiBranco <66792365+KauntiBranco@users.noreply.github.com>
2024-04-21 18:46:53 +0100

    Delete configs directory

--- configs/db.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  database: "Hotel Eden",
-  server: "localhost",
-  driver: "SQL Server Native Client 11.0",
-  options: {
-    instanceName: "MSSQLlocaldb",
-    trustedConnection: true
-  }
-}
